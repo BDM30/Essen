82fd66a6f6c7b193c9c5016ad6fcc886fec9763f
Vladislaff Evtushenko <starson4587@gmail.com>
2015-10-22 22:54:45 +0300

    - Добавил все 4 предполагаемых видов запросов.

--- src/Essen/Controllers/UserController.cs
+++ src/Essen/Controllers/UserController.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Essen.Models;
 using Essen.Models.Entities;
 using Microsoft.AspNet.Mvc;
 
+/*
+Пример CRUD контроллера с 4мя сущностями
+*/
+
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+// guide to use postman
 
 namespace Essen.Controllers
 {
@@ -46,14 +50,27 @@ namespace Essen.Controllers
 
     // PUT api/values/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public void Put(int id, [FromBody] User user)
     {
+      if (user.UserID == 0)
+        user.UserID = id;
+      data.Users.Update(user);
+      data.SaveChanges();
     }
 
     // DELETE api/values/5
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
+      User user = (from x in data.Users
+        where x.UserID == id
+        select x).FirstOrDefault();
+      if (user != null)
+      {
+        data.Users.Remove(user);
+        data.SaveChanges();
+      } 
     }
+
   }
 }
